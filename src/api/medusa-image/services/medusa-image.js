3f26187efda71cf8817e5102dfb442e1919c4bc1
Paweł Niedźwiecki <hello@uxu.pl>
2022-05-31 15:36:08 +0200

    i added api for image & component for metadate image

---
+++ src/api/medusa-image/services/medusa-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * medusa-image service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::medusa-image.medusa-image');
