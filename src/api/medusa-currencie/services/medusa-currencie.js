9fbe28495320c47cc4556003d93e51b41d107640
Paweł Niedźwiecki <hello@uxu.pl>
2022-06-01 15:57:27 +0200

    i added medusa-currencie

---
+++ src/api/medusa-currencie/services/medusa-currencie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * medusa-currencie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::medusa-currencie.medusa-currencie');
