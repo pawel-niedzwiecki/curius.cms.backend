150534543ea3b68cbfc36fee94bfcc229ea84368
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-15 14:05:13 +0200

    i added route & controlerr for shop-sync-data-history

--- src/plugins/strapi-iv-plugin-medusa/server/routes/admin-api/shop-sync-data/index.js
+++ src/plugins/strapi-iv-plugin-medusa/server/routes/admin-api/shop-sync-data/index.js
@@ -3,6 +3,14 @@
 module.exports = [
   {
     method: "GET",
+    path: "/shop-sync-data-history",
+    handler: "/shop-sync-data-history.all",
+    config: {
+      policies: ["admin::isAuthenticatedAdmin"],
+    },
+  },
+  {
+    method: "GET",
     path: "/shop-sync-data-all",
     handler: "/shop-sync-data.syncAll",
     config: {
---
+++ src/plugins/strapi-iv-plugin-medusa/server/services/shop-sync-data-history/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  async all() {
+    return { ok: "pkl" };
+  },
+};
