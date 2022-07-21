52b1294e03d5014cc7d947960406ba045d8ffc08
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-21 15:09:38 +0200

    i added service for clears status sync

--- src/plugins/strapi-iv-plugin-medusa/server/services/shop-sync-data/index.js
+++ src/plugins/strapi-iv-plugin-medusa/server/services/shop-sync-data/index.js
@@ -11,6 +11,19 @@ module.exports = {
         .query("plugin::strapi-iv-plugin-medusa.shop-sync-historry")
         .create({ data: { status: "pending", type: "all" } });
 
+    await strapi.plugin("strapi-iv-plugin-medusa").service("shop-sync-data").clearStatusSyncAll();
+
     return { status: "pending" };
+  },
+
+  async clearStatusSyncAll() {
+    const statusSync = await strapi.query("plugin::strapi-iv-plugin-medusa.shop-sync-status").findMany();
+
+    if (!!statusSync?.length)
+      statusSync.forEach((item) =>
+        (async () => await strapi.entityService.delete("plugin::strapi-iv-plugin-medusa.shop-sync-status", item.id))()
+      );
+
+    return { status: "resolve" };
   },
 };
